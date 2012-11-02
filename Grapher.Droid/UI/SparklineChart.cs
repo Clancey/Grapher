aaedf5b7234d39317235a266b5c665f42af447d9
Clancey <james.clancey@gmail.com>
2012-11-01 21:33:37 -0700

    Fixed the graph

--- Grapher.Droid/UI/SparklineChart.cs
+++ Grapher.Droid/UI/SparklineChart.cs
@@ -6,7 +6,7 @@ using Android.Content;
 using Android.Content.Res;
 using Android.Graphics;
 using Android.Views;
-using System.Threading;
+using System.Timers;
 
 namespace Grapher
 {
@@ -43,11 +43,11 @@ namespace Grapher
 
 			for (int i = 0; i < this.data.Count(); i++)
 			{
-				float value = this.data[i];
+				float value = this.data[i].Value;
 				if (value < min)
-					min = value;
+					this.min = value;
 				if (value > max)
-					max = value;
+					this.max = value;
 			}
 		}
 
@@ -75,7 +75,7 @@ namespace Grapher
 
 		protected override void OnDraw (Canvas canvas)
 		{
-			if (this.data == null || this.data.Length == 0)
+			if (this.data == null || this.data.Count() == 0)
 			{
 				base.OnDraw (canvas);
 				return;
@@ -87,25 +87,18 @@ namespace Grapher
 				this.paint.SetStyle (Paint.Style.Stroke);
 			}
 
-			Color color = LineColor;
-			if (color == default (Color))
-			{
-				TypedArray attrs = Context.Theme.ObtainStyledAttributes (new[] { Resource.Attribute.TextColorPrimary });
-				this.paint.Color = attrs.GetColor (0, 0xFFFFFF);
-			}
-			else
-				this.paint.Color = color;
+			this.paint.Color = LineColor;
 
 			float mx = this.max;
 			float yscale = canvas.Height / (mx - this.min);
 			float xscale = (float)canvas.Width / 60;
 
 			Path path = new Path();
-			path.MoveTo (0, (mx - this.data[0]) * yscale);
+			path.MoveTo (0, (mx - this.data[0].Value) * yscale);
 
-			for (int i = 1; i < this.data.Length; i++)
+			for (int i = 1; i < this.data.Count(); i++)
 			{
-				var telemetry = d[i];
+				var telemetry = data[i];
 				var x = ((float)(telemetry.Timestamp - DateTime.Now).TotalSeconds * xscale) + (float)canvas.Width;
 				var y = (mx - telemetry.Value) * yscale;
 				path.LineTo (x,y);
